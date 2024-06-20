04e832ee253828c161c9bfdd2c6b5f6381862685
emmanuel <zundercoc@gmail.com>
2024-06-20 17:46:28 +0200

    Funcionalidad de Guardar Producto Create y logger para la clase ProductoController

--- src/main/java/com/curso/ecommerce/spring_ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/controller/ProductoController.java
@@ -1,16 +1,46 @@
 package com.curso.ecommerce.spring_ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.curso.ecommerce.spring_ecommerce.model.Producto;
+import com.curso.ecommerce.spring_ecommerce.model.Usuario;
+import com.curso.ecommerce.spring_ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 
+    private final Logger logger = LoggerFactory.getLogger(ProductoController.class);
+
+    @Autowired
+    private ProductoService productoService;
+
     @GetMapping
-        public String show(){
-            System.out.println("\nshow");
-            return "productos/show";
-        }
+    public String show() {
+
+        return "productos/show";
+    }
+
+    @GetMapping("/create")
+    public String create() {
+
+        return "productos/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto) {
+        logger.info("\nEste es el objeto producto{}",producto);
+        Usuario user = new Usuario(1, "", "", "", "", "", "", "");
+        producto.setUsuario(user);
+        productoService.save(producto);
+        return "redirect:/productos";
+        
+
+    }
 }
--- src/main/java/com/curso/ecommerce/spring_ecommerce/model/Usuario.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/model/Usuario.java
@@ -32,4 +32,17 @@ public class Usuario {
     private List<Producto> productos;
     @OneToMany(mappedBy = "usuario")
     private List<Orden> ordenes;
+    public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono,
+            String tipo, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.username = username;
+        this.email = email;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.tipo = tipo;
+        this.password = password;
+    }
+
+    
 }
