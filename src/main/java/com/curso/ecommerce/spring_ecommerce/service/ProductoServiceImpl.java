793774eee3173faeda1ed5616d71ae3d1ebee1d2
emmanuel <zundercoc@gmail.com>
2024-06-20 18:00:04 +0200

    funcionalidad mostrar productos para el administrador

--- src/main/java/com/curso/ecommerce/spring_ecommerce/service/ProductoServiceImpl.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/service/ProductoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.spring_ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,11 @@ public class ProductoServiceImpl implements ProductoService{
     @Override
     public void update(Producto producto) {
      productoRepository.save(producto); 
+    }
+
+    @Override
+    public List<Producto> findAll() {
+        return productoRepository.findAll();
     }
 
 }
