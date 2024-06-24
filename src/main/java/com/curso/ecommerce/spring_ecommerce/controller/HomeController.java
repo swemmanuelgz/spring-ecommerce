75538890a83a7f20c5232361ba250efd4cd65361
emmanuel <zundercoc@gmail.com>
2024-06-24 13:21:58 +0200

    validacion para doble producto y ver el carrito desde la cabecera del template del usuario , menu opcion carrito

--- src/main/java/com/curso/ecommerce/spring_ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/controller/HomeController.java
@@ -68,7 +68,14 @@ public class HomeController {
             detalleOrden.setTotal(producto.getPrecio()*cantidad);
             detalleOrden.setProducto(producto);
 
-            detalles.add(detalleOrden);
+            //validar que el producto no se añada dos veces
+            Integer idProducto = producto.getId();
+            boolean ingresado = detalles.stream().anyMatch( p -> p.getProducto().getId()==id);
+
+            if (!ingresado) {
+                detalles.add(detalleOrden);
+            }
+
             sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
             orden.setTotal(sumaTotal);
@@ -93,6 +100,13 @@ public class HomeController {
             sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
             orden.setTotal(sumaTotal);
+            model.addAttribute("cart", detalles);
+            model.addAttribute("orden", orden);
+            return "usuario/carrito";
+        }
+
+        @GetMapping("/getCart")
+        public String getCart(Model model){
             model.addAttribute("cart", detalles);
             model.addAttribute("orden", orden);
             return "usuario/carrito";
