7460bc0b61bec060962e4a6757ed47f4fc93d3cc
emmanuel <zundercoc@gmail.com>
2024-06-26 12:07:13 +0200

    implementamos template diferente para usuario logueado

--- src/main/java/com/curso/ecommerce/spring_ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/controller/HomeController.java
@@ -57,6 +57,9 @@ public class HomeController {
             log.info(ProductoController.ANSI_GREEN+"\nSesion de usuario: {}",session.getAttribute("idusuario")+ProductoController.ANSI_RESET);
             model.addAttribute("productos", productoService.findAll());
 
+            //session
+            model.addAttribute("sesion",session.getAttribute("idusuario"));
+
             return "usuario/home";
         }
 
@@ -123,10 +126,13 @@ public class HomeController {
         }
 
         @GetMapping("/getCart")
-        public String getCart(Model model){
+        public String getCart(Model model,HttpSession session){
 
             model.addAttribute("cart", detalles);
             model.addAttribute("orden", orden);
+
+            //session
+            model.addAttribute("sesion", session.getAttribute("idusuario"));
             return "/usuario/carrito";
         }
 
