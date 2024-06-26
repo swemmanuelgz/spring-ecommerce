5275b32a31ce2f7c51ee36e0ee8e876da4c1405e
emmanuel <zundercoc@gmail.com>
2024-06-26 17:20:21 +0200

    funcionalidad para ver ordenes desde ADMIN

--- src/main/java/com/curso/ecommerce/spring_ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/controller/AdministradorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.spring_ecommerce.model.Producto;
+import com.curso.ecommerce.spring_ecommerce.service.IOrdenService;
 import com.curso.ecommerce.spring_ecommerce.service.IUsuarioService;
 import com.curso.ecommerce.spring_ecommerce.service.ProductoService;
 
@@ -20,6 +21,8 @@ public class AdministradorController {
     private ProductoService productoService;
     @Autowired
     private IUsuarioService usuarioService;
+    @Autowired
+    private IOrdenService ordenService;
 
     @GetMapping
     public String home(Model model) {
@@ -31,5 +34,11 @@ public class AdministradorController {
     public String usuarios(Model model){
         model.addAttribute("usuarios", usuarioService.findAll());
         return "administrador/usuarios";
+    }
+
+    @GetMapping("/ordenes")
+    public String ordenes(Model model){
+        model.addAttribute("ordenes", ordenService.findAll());
+        return "administrador/ordenes";
     }
 }
