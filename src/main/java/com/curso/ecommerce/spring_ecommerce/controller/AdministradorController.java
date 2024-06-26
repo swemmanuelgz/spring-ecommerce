55ae85117432defebe65d59ae14d4c4be793ece1
emmanuel <zundercoc@gmail.com>
2024-06-26 16:45:40 +0200

    funcionalidad ver usuarios Admin

--- src/main/java/com/curso/ecommerce/spring_ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/controller/AdministradorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.spring_ecommerce.model.Producto;
+import com.curso.ecommerce.spring_ecommerce.service.IUsuarioService;
 import com.curso.ecommerce.spring_ecommerce.service.ProductoService;
 
 @Controller
@@ -17,11 +18,18 @@ public class AdministradorController {
     
     @Autowired
     private ProductoService productoService;
+    @Autowired
+    private IUsuarioService usuarioService;
 
     @GetMapping
     public String home(Model model) {
         List<Producto> productos = productoService.findAll();
         model.addAttribute("productos", productos);
         return "administrador/home";
+    }
+    @GetMapping("/usuarios")
+    public String usuarios(Model model){
+        model.addAttribute("usuarios", usuarioService.findAll());
+        return "administrador/usuarios";
     }
 }
--- src/main/java/com/curso/ecommerce/spring_ecommerce/service/IUsuarioService.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/service/IUsuarioService.java
@@ -1,10 +1,12 @@
 package com.curso.ecommerce.spring_ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.curso.ecommerce.spring_ecommerce.model.Usuario;
 
 public interface IUsuarioService {
+    List<Usuario> findAll();
     Optional<Usuario> findById(Integer id);
     Usuario save(Usuario usuario);
     Optional<Usuario> findByEmail(String email);
